cecc18f9e7a655168f707713fafd8a41b45ecf8d
Martin Barria Joost <martinjoostw@gmail.com>
2020-09-12 12:10:50 -0600

    base template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
+import { NotfoundComponent } from "./pages/notfound/notfound.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { BreadcrumbsComponent } from "./shared/breadcrumbs/breadcrumbs.component";
+import { SidebarComponent } from "./pages/sidebar/sidebar.component";
+import { HeaderComponent } from "./pages/header/header.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotfoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
