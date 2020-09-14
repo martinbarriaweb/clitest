9b64de990ee35e3abf7513225461169be56ce9e4
Martin Barria Joost <martinjoostw@gmail.com>
2020-09-14 11:26:10 -0600

    feat(progress): styles

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-progress',
-  templateUrl: './progress.component.html',
-  styles: []
+  selector: "app-progress",
+  templateUrl: "./progress.component.html",
+  styleUrls: ["progress.component.css"],
 })
 export class ProgressComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
