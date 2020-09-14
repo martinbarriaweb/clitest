b4cc7b5ccb10307047ab33777fd95e3cb3b84fdb
Martin Barria Joost <martinjoostw@gmail.com>
2020-09-14 09:53:14 -0600

    chore(modules): pages and shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,21 @@ import { NgModule } from "@angular/core";
 
 // MODULES
 import { AppRoutingModule } from "./app-routing.module";
+import { PagesModule } from "./pages/pages.module";
 
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./auth/login/login.component";
-import { RegisterComponent } from "./auth/register/register.component";
-import { NotfoundComponent } from "./pages/notfound/notfound.component";
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { BreadcrumbsComponent } from "./shared/breadcrumbs/breadcrumbs.component";
-import { ProgressComponent } from "./pages/progress/progress.component";
-import { Graphics1Component } from "./pages/graphics1/graphics1.component";
-import { SidebarComponent } from "./shared/sidebar/sidebar.component";
-import { HeaderComponent } from "./shared/header/header.component";
-import { PagesComponent } from "./pages/pages.component";
+import { RegisterComponent } from "./auth/register/register.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    NotfoundComponent,
     DashboardComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Graphics1Component,
-    PagesComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, PagesModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
