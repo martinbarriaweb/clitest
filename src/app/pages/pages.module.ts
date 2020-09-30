25e60836e99bd54469fbcbd53cdf605eb7a0d3db
Martin Barria Joost <martinjoostw@gmail.com>
2020-09-29 21:39:18 -0600

    feat(accountsettings): base

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,16 +10,27 @@ import { ComponentsModule } from "../components/components.module";
 import { ProgressComponent } from "./progress/progress.component";
 import { Graphics1Component } from "./graphics1/graphics1.component";
 import { PagesComponent } from "./pages.component";
+import { AccountSettingsComponent } from "./account-settings/account-settings.component";
 
 @NgModule({
-  declarations: [ProgressComponent, Graphics1Component, PagesComponent],
-  exports: [ProgressComponent, Graphics1Component, PagesComponent],
+  declarations: [
+    ProgressComponent,
+    Graphics1Component,
+    PagesComponent,
+    AccountSettingsComponent,
+  ],
+  exports: [
+    ProgressComponent,
+    Graphics1Component,
+    PagesComponent,
+    AccountSettingsComponent,
+  ],
   imports: [
     FormsModule,
     RouterModule,
     CommonModule,
     SharedModule,
-    ComponentsModule
+    ComponentsModule,
   ],
 })
-export class PagesModule { }
+export class PagesModule {}
