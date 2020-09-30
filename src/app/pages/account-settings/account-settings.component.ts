872d63bfc37256cb3600c732e5bef836850f7178
Martin Barria Joost <martinjoostw@gmail.com>
2020-09-29 22:12:19 -0600

    chore(account-settings): select theme localstorage

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { link } from "fs";
 
 @Component({
-  selector: 'app-account-settings',
-  templateUrl: './account-settings.component.html',
-  styleUrls: ['./account-settings.component.css']
+  selector: "app-account-settings",
+  templateUrl: "./account-settings.component.html",
+  styleUrls: ["./account-settings.component.css"],
 })
 export class AccountSettingsComponent implements OnInit {
+  public linkTheme = document.querySelector("#theme");
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  changeTheme(theme: string) {
+    const urlTheme = `./assets/css/colors/${theme}.css`;
+    this.linkTheme.setAttribute("href", urlTheme);
+    localStorage.setItem("theme", theme);
   }
-
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-pages',
-  templateUrl: './pages.component.html',
-  styles: []
+  selector: "app-pages",
+  templateUrl: "./pages.component.html",
+  styles: [],
 })
 export class PagesComponent implements OnInit {
-
-  constructor() { }
+  public linkTheme = document.querySelector("#theme");
+  constructor() {}
 
   ngOnInit() {
+    this.setTheme();
   }
 
+  setTheme() {
+    const theme = localStorage.getItem("theme") || "purple";
+    this.linkTheme.setAttribute("href", `./assets/css/colors/${theme}.css`);
+  }
 }
